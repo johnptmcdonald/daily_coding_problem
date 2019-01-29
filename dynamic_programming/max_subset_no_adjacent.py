0350436777f72768689668f2b6d647577d35ea5d
johnptmcdonald <johnptmcdonald@gmail.com>
2019-01-29 18:18:35 -0500

    n time 1 space  max subset soln

--- dynamic_programming/max_subset_no_adjacent.py
+++ dynamic_programming/max_subset_no_adjacent.py
@@ -31,6 +31,27 @@ def maxSubsetSumNoAdjacent(lst):
 
 	return dp[-1]
 
+# 3 - Even better - we don't need the whole dp array, just the previous two numbers
+def maxSubsetSumNoAdjacent(lst):
+	# for every number I can either take it, OR the next one. I want to use DP to find the max up to that point.
+	if len(lst) == 0:
+		return 0
+	if len(lst) == 1:
+		return lst[0]
+
+	dp = [lst[0], max(lst[0], lst[1])]
+	
+	for i in range(2, len(lst)):
+		# max is either...
+		# the max up to the one before last plus current 
+		# OR the max up to the last one
+		curr_largest = max(dp[0]+lst[i], dp[1])
+		dp[0] = dp[1]
+		dp[1] = curr_largest
+
+	return dp[-1]
+
+
 maxSubsetSumNoAdjacent([75,105,120,75,90,135])
 
 
--- dynamic_programming/problems.md
+++ dynamic_programming/problems.md
@@ -1,1 +1,26 @@
-problems.md
+Knapsack
+Equal subset sum partition
+subset sum
+minimum subset sum
+count of subset sum
+target sum
+
+Unbounded knapsack
+rod cutting
+coin change
+minimum coin change
+maximum ribbon cut
+
+Longest palindromic subsequence
+longest palindromic substring
+count of palindromic substirngs
+minimum deletions in a string to make it a palindrome
+palindromic partitioning
+
+Fib numbers
+staircase
+number factors
+minimum jumps to reach end
+minimum jumps with fee
+house thief
+
