2b410297591c9ac292ab976347571b843c007df6
johnptmcdonald <johnptmcdonald@gmail.com>
2019-01-30 10:07:25 -0500

    nicer min_nums_for_change

--- dynamic_programming/min_num_coins_for_change.py
+++ dynamic_programming/min_num_coins_for_change.py
@@ -2,37 +2,53 @@
 # and a single target, find the minimum number of coins to make that target
 
 # bleughs 
+# def min_number_of_coins_for_change(n, denoms):
+# 	denoms = sorted(denoms)
+# 	dp = [0] * (n+1)
+
+# 	for denom in denoms:
+# 		for amount in range(denom, n+1):
+			
+# 			temp2 = dp[amount]
+
+# 			num_current_denom = amount//denom
+# 			remainder = amount%denom
+
+# 			temp = dp[amount]
+# 			dp[amount] = num_current_denom
+
+# 			if dp[remainder] == 0:
+# 				if remainder != 0:
+# 					dp[amount] = 0
+
+# 			dp[amount] += dp[remainder]
+
+# 			if dp[amount] == 0:
+# 				dp[amount] = temp
+
+# 			if dp[amount] > temp2 and temp2 is not 0:
+# 				dp[amount] = temp2
+
+# 	return dp[n]
+
+
+# muuuuch nicer
 def min_number_of_coins_for_change(n, denoms):
-	denoms = sorted(denoms)
-	dp = [0] * (n+1)
+
+	dp = [float('inf')] * (n+1)
+	dp[0] = 0
 
 	for denom in denoms:
-		for amount in range(denom, n+1):
-			
-			temp2 = dp[amount]
+		for amount in range(1, n+1):
+			if denom <= amount:
+				dp[amount] = min(dp[amount], 1 + dp[amount-denom])
 
-			num_current_denom = amount//denom
-			remainder = amount%denom
 
-			temp = dp[amount]
-			dp[amount] = num_current_denom
+	print(dp)
+	return dp[-1]
 
-			if dp[remainder] == 0:
-				if remainder != 0:
-					dp[amount] = 0
-
-			dp[amount] += dp[remainder]
-
-			if dp[amount] == 0:
-				dp[amount] = temp
-
-			if dp[amount] > temp2 and temp2 is not 0:
-				dp[amount] = temp2
-
-	return dp[n]
 
 min_number_of_coins_for_change(3, [2,1])
-
 
 
 
