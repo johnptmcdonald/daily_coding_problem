73ae4d2e39799f09a69f4aab10bdc5349ddd2da0
johnptmcdonald <johnptmcdonald@gmail.com>
2019-02-07 20:07:40 -0500

    adds the old balanced brackets

---
+++ stacks/balanced_brackets.py
@@ -0,0 +1,26 @@
+# The classic!
+
+def balancedBrackets(string):
+	stack = []
+
+	matchingBrackets = {
+		')': '(',
+		']': '[',
+		'}': '{'
+	}
+
+	for bracket in string:
+		if bracket in matchingBrackets.values():
+			stack.append(bracket)
+		else:
+			if len(stack) == 0:
+				return False
+			if matchingBrackets[bracket] == stack[-1]:
+				stack.pop()
+			else:
+				return False
+
+	return len(stack) == 0
+
+
+print(balancedBrackets("(({{}}))"))
