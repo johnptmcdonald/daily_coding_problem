5372ab6328ade4368eda7b6cf369c3bd41db6799
johnptmcdonald <johnptmcdonald@gmail.com>
2019-03-25 14:05:06 -0400

    adds longest distinct substring

---
+++ 012_longest_substring_distinct_chars.py
@@ -0,0 +1,34 @@
+# Given an integer k and a string s, find the length of the longest substring that contains at most k distinct characters.
+
+# For example, given s = "bcba" and k = 2, the longest substring with k distinct characters is "bcb".
+
+def longest_substring(str, k):
+	p1 = 0
+	p2 = 0
+
+	distincts = {
+		str[p2] = True
+	}
+
+	# delete distincts['a']
+
+	length = 0
+
+	while p2 < len(str) - 1:
+		if len(distincts) >= k+1:
+			old_s = str[p1]
+			p1 = p1 + 1
+			new_s = str[p1]
+
+			if old_s != new_s:
+				del distincts[old_s]
+
+		if distincts <= k:
+			old_s = str[p2]
+			p2 = p2 + 1
+			new_s = str[p2]
+
+			if old_s != new_s:
+				distincts[new_s] = True
+
+	return len(distincts)
