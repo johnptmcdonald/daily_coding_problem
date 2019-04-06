96599599b1e06567c49cf9dd056d191aa488a919
johnptmcdonald <johnptmcdonald@gmail.com>
2019-04-06 15:53:43 -0400

    add anagram indices

--- dcp_book/2-strings/2.1_find_anagram_indices.py
+++ dcp_book/2-strings/2.1_find_anagram_indices.py
@@ -1,1 +1,10 @@
-2.1_find_anagram_indices.py
+
+# Given a word w and a string s, find all indices in s which are the starting locations of anagrams of w
+
+w = 'ab'
+s = 'abxaba'
+
+# output = [0, 3, 4]
+
+def find_anagram_indices(w, s):
+	pass
