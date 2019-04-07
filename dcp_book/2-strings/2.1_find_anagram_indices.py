889fcf9f2a19714bf5e4d2c96dc9f021189347e9
johnptmcdonald <johnptmcdonald@gmail.com>
2019-04-07 14:04:27 -0400

    add comments to find anagram indices

--- dcp_book/2-strings/2.1_find_anagram_indices.py
+++ dcp_book/2-strings/2.1_find_anagram_indices.py
@@ -1,10 +1,52 @@
+from collections import defaultdict
 
 # Given a word w and a string s, find all indices in s which are the starting locations of anagrams of w
 
-w = 'ab'
+word = 'ab'
 s = 'abxaba'
 
 # output = [0, 3, 4]
 
-def find_anagram_indices(w, s):
-	pass
+def del_if_zero(dict, char):
+	if dict[char] == 0:
+		del dict[char]
+
+def find_anagram_indices(word, s):
+	result = []
+
+	freq = defaultdict(int)
+
+	for char in word:
+		freq[char] += 1
+
+	for char in s[:len(word)]:
+		freq[char] -= 1
+		del_if_zero(freq, char)
+
+	if not freq:
+		result.append(0)
+
+	for i in range(len(word), len(s)):
+
+		start_char, end_char = s[i - len(word)], s[i]
+		print()
+
+		print(i, start_char, end_char)
+		freq[start_char] += 1
+		del_if_zero(freq, start_char)
+
+		freq[end_char] -= 1
+		del_if_zero(freq, end_char)
+
+		if not freq:
+			beginning_index = i - len(word) + 1
+			print('empty dict', beginning_index)
+			result.append(beginning_index)
+
+	print(result)
+	return result
+
+find_anagram_indices(word, s)
+
+
+
