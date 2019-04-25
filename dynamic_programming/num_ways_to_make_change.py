930e88acb9e72816ce9a3d18ee9a2cfe26ea7563
johnptmcdonald <johnptmcdonald@gmail.com>
2019-04-25 08:47:48 -0400

    adds steps to num_ways_to_make_change

--- dynamic_programming/num_ways_to_make_change.py
+++ dynamic_programming/num_ways_to_make_change.py
@@ -13,8 +13,39 @@ def number_of_ways_to_make_change(n, denominations):
 		for amount in range(1, n+1):
 			if denom <= amount:
 				dp[amount] += dp[amount-denom]
+		print(dp)
 
+	print(dp[n])
 	return dp[n]
 
 
+	
+
+
+
+
 number_of_ways_to_make_change(6, [1,5]) # => 2
+
+
+
+
+# Step 1: How to recognize a Dynamic Programming problem. ...
+	# the problems has subproblems that can be used to solve the whole problem
+
+# Step 2: Identify problem variables. ...
+	# amount, denom
+
+# Step 3: Clearly express the recurrence relation. ...
+	# num_ways(amount, denom) = 1 + num_ways(amount - denom) IF denom < amount
+
+# Step 4: Identify the base cases. ...
+	# num_ways(0, any) === 0
+
+# Step 5: Decide if you want to implement it iteratively or recursively. ...
+	# iteratively
+# Step 6: Add memoization. ...
+	# dp is an array 
+
+# Step 7: Determine Time complexity.
+
+
